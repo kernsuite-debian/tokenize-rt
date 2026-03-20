0459eb4a9c49cd9b42240290ca730bcdc922b035
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2026-03-20 15:40:56 +0100

    New upstream version 6.2.0

--- tokenize_rt.py
+++ tokenize_rt.py
@@ -6,11 +6,11 @@ import keyword
 import re
 import sys
 import tokenize
-from typing import Generator
-from typing import Iterable
+from collections.abc import Generator
+from collections.abc import Iterable
+from collections.abc import Sequence
+from re import Pattern
 from typing import NamedTuple
-from typing import Pattern
-from typing import Sequence
 
 # this is a performance hack.  see https://bugs.python.org/issue43014
 if (  # pragma: no branch
@@ -46,6 +46,16 @@ class Token(NamedTuple):
 
 _string_re = re.compile('^([^\'"]*)(.*)$', re.DOTALL)
 _escaped_nl_re = re.compile(r'\\(\n|\r\n|\r)')
+
+NAMED_UNICODE_RE = re.compile(r'(?<!\\)(?:\\\\)*(\\N\{[^}]+\})')
+
+
+def curly_escape(s: str) -> str:
+    parts = NAMED_UNICODE_RE.split(s)
+    return ''.join(
+        part.replace('{', '{{').replace('}', '}}') if i % 2 == 0 else part
+        for i, part in enumerate(parts)
+    )
 
 
 def _re_partition(regex: Pattern[str], s: str) -> tuple[str, str, str]:
@@ -99,6 +109,13 @@ def src_to_tokens(src: str) -> list[Token]:
             end_offset += len(newtok.encode())
 
         tok_name = tokenize.tok_name[tok_type]
+
+        if tok_name in {'FSTRING_MIDDLE', 'TSTRING_MIDDLE'}:  # pragma: >=3.12 cover  # noqa: E501
+            if '{' in tok_text or '}' in tok_text:
+                new_tok_text = curly_escape(tok_text)
+                ecol += len(new_tok_text) - len(tok_text)
+                tok_text = new_tok_text
+
         tokens.append(Token(tok_name, tok_text, sline, end_offset))
         last_line, last_col = eline, ecol
         if sline != eline:
@@ -115,7 +132,7 @@ def tokens_to_src(tokens: Iterable[Token]) -> str:
 
 def reversed_enumerate(
         tokens: Sequence[Token],
-) -> Generator[tuple[int, Token], None, None]:
+) -> Generator[tuple[int, Token]]:
     for i in reversed(range(len(tokens))):
         yield i, tokens[i]
 
